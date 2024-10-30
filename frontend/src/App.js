c8e5ef4ee18725437ee8c4e1e4f1d8e2fb130eff
MdAqib184 <mdaqib184@gmail.com>
2024-10-30 14:21:45 +0530

    update

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,7 +9,7 @@ function App() {
     const [uploadedFileName, setUploadedFileName] = useState(null); // Track the uploaded file name
 
     // API base URL for FastAPI backend
-    const API_BASE_URL = "https://querypdf-8s2s.onrender.com/";
+    const API_BASE_URL = "https://querypdf-8s2s.onrender.com";
 
     const handleFileUpload = async (file) => {
         try {
